9a83297a46c2b61b777223db3346b7d2a071a01c
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2024-04-25 02:05:36 +0530

    Pushing Code files

---
+++ script.js
@@ -0,0 +1,18 @@
+// Used GSAP for delayed and smooth animation of pop-up text
+
+gsap.from(".delay1",{
+    opacity:0,
+    repeat:0,
+    delay:0.8,
+    duration:1,
+    y:60
+}
+) 
+gsap.from(".delay",{
+    opacity:0,
+    repeat:0,
+    delay:1,
+    duration:1,
+    y:60
+}
+)
